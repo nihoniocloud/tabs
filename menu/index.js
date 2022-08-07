edc06b7937f1a04250e2fbe3a1cf6a8b08491aef
nihonio-cloud <110445763+cloud-nihonio-uploads@users.noreply.github.com>
2022-08-07 06:09:03 -0300

    Update index.js

--- menu/index.js
+++ menu/index.js
@@ -1,8 +1,11 @@
-function copyToClipboard(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
-  document.execCommand("copy");
-  console.log('Copied.');
-  $temp.remove();
+function copyToClipboard(input) {
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(input).then(() => {
+      console.log('Copied to clipboard successfully.');
+    }, (err) => {
+      console.log('Failed to copy the text to clipboard.', err);
+    });
+  } else if (window.clipboardData) {
+    window.clipboardData.setData("Text", input);
+  }
 }
