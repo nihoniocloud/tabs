71743f98045ca2dfd90d7bb778189589fc0a3f48
nihonio-cloud <110445763+cloud-nihonio-uploads@users.noreply.github.com>
2022-08-09 02:16:02 -0300

    Update oh.js

--- oh.js
+++ oh.js
@@ -1,15 +1,2 @@
-function getQueryStringParam(param) {
-  var url = window.location.toString();
-  url.match(/\?(.+)$/);
-  var params = RegExp.$1;
-  params = params.split("&");
-  var queryStringList = {};
-  for(var i = 0; i < params.length; i++) {
-    var tmp = params[i].split("=");
-    queryStringList[tmp[0]] = unescape(tmp[1]);
-  }
-  return decodeURIComponent(queryStringList[param]);
-}
-let qParam = getQueryStringParam('q').split('+').join(' ');
 const div = document.getElementById('uText');
-div.innerHTML = qParam;
+div.innerHTML = ('Ok');
